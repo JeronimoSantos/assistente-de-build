ba617e5bac9feb18a7a1df8ba6c634054aff0685
Jerônimo Santos Rodrigues <santosominorej@gmail.com>
2025-07-12 00:23:59 -0300

    :sparkles: feat: Adicionando pasta e arquivo js

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+//
+const apiKeyInput = document.getElementById('apiKey');
+const gameSelect = document.getElementById('gameSelect');
+const questionInput = document.getElementById('questionInput');
+const askButton = document.getElementById('askButton');
+const form = document.getElementById('form');
+const aiResponse = document.getElementById('aiResponse');
+
+//
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+})
+
